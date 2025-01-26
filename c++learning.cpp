5a6fd1cf0f29b0892f9cf3df23810ab90dc16685
muhammadhuzaifaqayyum <connectalhuzaifiii@gmail.com>
2025-01-26 21:29:15 +0500

    1st practice

---
+++ c++learning.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // cout << "Hello, World!" << endl;
+    // cout << "I'm Muhammad Huzaifa Qayyum";
+
+    // int markInMaths = 90;
+    // cout << "I'm Just testing this " << markInMaths;
+
+    // int b;
+    // long a;
+    // long long c;
+    // float score = 3.4;
+    // double score2 = 3.4;
+    // long double score3 = 3.4;
+    // score = 3.4;
+    // cout << "Size of int: " << endl;
+
+    // int a, b;
+    // cout << "Enter First Number";
+    // cin >> a;
+    // cout << "Enter Second Number";
+    // cin >> b;
+    // cout << "The sum of two numbers is: " << a + b;
+
+    // If else Case practice
+
+    // int age;
+    // cout << "Enter your age: ";
+    // cin >> age;
+    // if (age >= 150 || age <= 0)
+    // {
+    //     cout << "You are not human being";
+    // }
+    // else if (age >= 18)
+    // {
+    //     cout << "You are eligible to vote";
+    // }
+    // else
+    // {
+    //     cout << "You are not eligible to vote";
+    // }
+
+    // switch case practice
+    // int age;
+    // cout << "Enter your age: ";
+    // cin >> age;
+    // switch (age)
+    // {
+    // case 12:
+    //     cout << "You are 12 years old";
+    //     break;
+    // case 13:
+    //     cout << "You are 13 years old";
+    //     break;
+
+    // default:
+    //     cout << "You are not 12 or 13 years old";
+    //     break;
+    // }
+
+    // While Loops Practice
+    // int index = 0;
+    // while (index < 34)
+    // {
+    //     cout << "The value of index is: " << index << endl;
+    //     index = index + 1;
+    // }
+
+    // Do While Loop Practice
+    // int index = 0;
+    // do
+    // {
+    //     cout << "The value of index is: " << index << endl;
+    //     index = index + 1;
+    // } while (index < 5);
+
+    // For Loop Practice
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "the value of i is: " << i << endl;
+    }
+    return 0;
+}
